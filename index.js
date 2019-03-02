0e94dca7ec6b6cc4c055639d1e65d08a05d583f9
Reed G <ellisonrg@gmail.com>
2019-03-02 03:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}!"
+  happyHolidaysTo('you')
+}
+
+
+function happyHolidayTo(holiday, name) {
+  return "Happy $(holiday), ${name}!"
+  happyHolidayTo('christmas')
+  happyHolidayTo('meredith')
+  
+}
+
+
+
+
+
+
+
+function sayHelloTo(firstName) {
+  console.log(`Hello, ${firstName}!`)
+}
+ 
+
+
+
+
+sayHelloTo('Isabel') // "Hello, Isabel!"
+sayHelloTo("Jane") // "Hello, Jane!"
+sayHelloTo("R2-D2") // "Hello, R2-D2!"
+sayHelloTo(1) // "Hello, 1!"
+ 
+// ^ Note that in the above, JavaScript coerces the number 1 to the string "1"
